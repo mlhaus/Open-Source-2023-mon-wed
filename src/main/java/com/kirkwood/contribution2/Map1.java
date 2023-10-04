736eddc74944cefc42e3120c5496d590e5c64ce0
Jennifer Nicewanner <jennifer-nicewanner@student.kirkwood.edu>
2023-10-04 14:40:33 -0500

    Completed Map1 and Map2

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapAB_Jennifer(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")) {
+            if(map.get("a").equals(map.get("b"))) {
+                map.remove("a");
+                map.remove("b");
+            }
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,16 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static Map<String, Integer> Jennifer_wordLen(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String str: strings) {
+            if(!map.containsKey(str)){
+                map.put(str, str.length());
+            }
+        }
+        return map;
+    }
 }
