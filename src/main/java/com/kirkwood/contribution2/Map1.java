13efab9a30cab2a233dc2325bb0c00c7e40c0db3
Jacob <jbrohr@icloud.com>
2023-10-04 14:22:33 -0500

    Add contributions Jacob Rohr

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,19 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+
+    public static Map<String, String> topping3_JacobRohr(Map<String, String> map) {
+        if(map.containsKey("potato") && !map.get("potato").equals(null)){
+            map.put("fries", map.get("potato"));
+
+        }
+        if(map.containsKey("salad") && !map.get("salad").equals(null)){
+            map.put("spinach", map.get("salad"));
+
+        }
+        return map;
+    }
+
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,18 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class Map2 {
+
+    public static Map<String, String> pairs_JacobRohr(String[] strings) {
+        Map<String, String> map = new HashMap<String, String>();
+        for(String strTemp: strings){
+            map.put(strTemp.substring(0,1), strTemp.substring(strTemp.length() - 1, strTemp.length()));
+        }
+        return map;
+    }
+
 }
