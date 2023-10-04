0178420eb1dfcdc3f7b5f820aa4b5f47099dd819
Tyler Barz <tyler-barz@student.kirkwood.edu>
2023-10-04 14:13:04 -0500

    Uploaded my map challenges to the files accordingly

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,17 @@
 package com.kirkwood.contribution2;
 
 public class Map1 {
+    public static Map<String, String> mapAB4_tyler(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")){
+            if (map.get("a").length() > map.get("b").length()){
+                map.put("c", map.get("a"));
+            } else if (map.get("b").length() > map.get("a").length()){
+                map.put("c", map.get("b"));
+            } else {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,14 @@
 package com.kirkwood.contribution2;
 
 public class Map2 {
+    public static Map<String, String> pairs_tyler(String[] strings) {
+        Map<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < strings.length; i++) {
+            String tmp = strings[i];
+            String first = String.valueOf(tmp.charAt(0));
+            String last = String.valueOf(tmp.charAt(tmp.length() - 1));
+            map.put(first, last);
+        }
+        return map;
+    }
 }
