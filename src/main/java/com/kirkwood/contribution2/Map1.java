2103542e10caa476b698459b91a2a70122ed65bb
Darrial729 <dlsjr0102@aol.com>
2023-10-04 14:16:34 -0500

    Darryl's Map1 and Map2 contributions

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,17 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapAB2_Darryl(Map<String, String> map) {
+
+        if(map.containsKey("a") && map.containsKey("b")){
+            if(map.get("a").equals(map.get("b"))){
+                map.remove("a");
+                map.remove("b");
+            }
+        }
+        return map;
+
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,24 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public String wordAppend_Darryl(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        String result = "";
+        for(int i = 0; i < strings.length; i++){
+            if(map.containsKey(strings[i])){
+                map.put(strings[i], map.get(strings[i]) +1);
+                if((map.get(strings[i]) % 2) == 0){
+                    result+= strings[i];
+                }
+            }
+            else{
+                map.put(strings[i], 1);
+            }
+
+        }
+        return result;
+    }
 }
