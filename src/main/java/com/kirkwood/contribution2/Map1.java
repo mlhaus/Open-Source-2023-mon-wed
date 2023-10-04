ac6c322520da5c80380d6092ed339d3cb485156d
Mitchell Stirmel <MStirmel02@Gmail.com>
2023-10-04 14:14:01 -0500

    Added map methods

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,19 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapAB4_MitchellStirmel(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            String a = map.get("a");
+            String b = map.get("b");
+            if      (a.length() > b.length()) map.put("c", a);
+            else if (b.length() > a.length()) map.put("c", b);
+            else {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,31 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static String wordAppend_MitchellStirmel(String[] strings) {
+
+        Map<String, Integer> map    = new HashMap<String, Integer>();
+        String               result = "";
+
+        for (int i = 0; i < strings.length; i++) {
+
+            String key = strings[i];
+
+            if (map.containsKey(key)) {
+                int val = map.get(key);
+                val++;
+                if (val % 2 == 0) {
+                    result += key;
+                }
+                map.put(key, val);
+            } else {
+                map.put(key, 1);
+            }
+
+        }
+
+        return result;
+    }
 }
