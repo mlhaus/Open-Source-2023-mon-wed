50f923aec45b0cffd408185590875c36d7cf49ec
Jared <jared.harvey10@gmail.com>
2023-10-04 14:14:38 -0500

    Second contribution

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,14 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapAB3_jared(Map<String, String> map) {
+        if(map.containsKey("a") && !map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        } else if(map.containsKey("b") && !map.containsKey("a")) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,22 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static String[] allSwap_jared(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < strings.length; i++) {
+            String currentChar = strings[i].substring(0,1);
+            if(map.containsKey(currentChar)) {
+                String temp = strings[i];
+                strings[i] = strings[map.get(currentChar)];
+                strings[map.get(currentChar)] = temp;
+                map.remove(currentChar);
+            } else {
+                map.put(currentChar, i);
+            }
+        }
+        return strings;
+    }
 }
