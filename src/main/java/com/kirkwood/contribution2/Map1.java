c45d2284bec2dff1842c0b883c9742235f71330f
Ben Collins <collinsbc98@gmail.com>
2023-10-04 15:08:05 -0500

    Adding map1 and map2 solutions from my codingbat. - Ben Collins

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapShare_BenCollins(Map<String, String> map) {
+        if(map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        if(map.containsKey("c")) {
+            map.remove("c");
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,16 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static Map<String, Integer> wordLen_BenCollins(String[] strings) {
+        Map<String, Integer> counter = new HashMap<>();
+        for(String element : strings) {
+            if(!counter.containsKey(element)) {
+                counter.put(element, element.length());
+            }
+        }
+        return counter;
+    }
 }
