56189e774fc43c90009b9f018d9cc630346ea9b5
Max Fare <max-fare@student.kirkwood.edu>
2023-10-04 14:17:06 -0500

    Completed my codingbat committments for the second contribution assignment.

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,19 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+
+    public static Map<String, String> mapAB3_max(Map<String, String> map) {
+        if(map.containsKey("a")){
+            if(!map.containsKey("b")){
+                map.put("b", map.get("a"));
+            }
+        }
+        else if(map.containsKey("b")){
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,25 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+
+    public static String[] firstSwap_max(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < strings.length; i++){
+            String str = String.valueOf(strings[i].charAt(0));
+            if(!map.containsKey(str)){
+                map.put(str, i);
+            }
+            else if(map.get(str) != -1){
+                String hold = strings[map.get(str)];
+                strings[map.get(str)] = strings[i];
+                strings[i] = hold;
+                map.put(str, -1);
+            }
+        }
+        return strings;
+    }
+
 }
