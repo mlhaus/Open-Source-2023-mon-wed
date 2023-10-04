5678f79e6ab7d30dc056c56ceac9f859d4938a6b
Andrew Larson <andrew-larson3@student.kirkwood.edu>
2023-10-04 14:14:47 -0500

    Andrew's Second Contribution Map-1 topping2 Map-2 pairs

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,16 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public Map<String, String> topping2_andrew(Map<String, String> map) {
+        if (map.containsKey("ice cream")) {
+            map.put("yogurt", map.get("ice cream")); // Set "yogurt" to the value of "ice cream"
+        }
+        if (map.containsKey("spinach")) {
+            map.put("spinach", "nuts"); // Change the value of "spinach" to "nuts"
+        }
+        return map;
+    }
+
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public Map<String, String> pairs_andrew(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for(String str: strings) {
+            map.put(str.substring(0, 1), str.substring(str.length()-1, str.length()));
+        }
+        return map;
+    }
+
 }
