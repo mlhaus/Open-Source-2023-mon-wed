62ac8a509e515b44569dba6d57f04382dd9af065
Christivie Mauwa <lisedjuma@gmail.com>
2023-10-04 14:26:12 -0500

    Contribution 2 assigment

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,13 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public Map<String, String> mapShareChristivie(Map<String, String> map) {
+        if(map.containsKey("a")){
+            map.put("b", map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
 }
