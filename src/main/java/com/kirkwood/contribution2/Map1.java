9fd36bfbd51380e262db5d8021225436b602d6b8
EVERETT DEVAUX <eardevaux@gmail.com>
2023-10-04 15:07:52 -0500

    Adding Map1 and Map2 problems from CodingBat

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,13 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> topping1_everett(Map<String, String> map) {
+        map.put("bread", "butter");
+        if(map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,14 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static Map<String, Integer> wordLen_everett(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+        for (String str :strings) {
+            map.put(str, str.length());
+        }
+        return map;
+    }
 }
