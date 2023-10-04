3ac7f76632d973e9c9f3033be45332e747cdacb0
Kate Rich <Kate.Rich.2014@gmail.com>
2023-10-04 15:49:11 -0500

    Completed CodingBat Contribution

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.*;
+
 public class Map1 {
+    public static Map<String, String> topping2_kate(Map<String, String> map) {
+        if(map.containsKey("ice cream") && !map.get("ice cream").isEmpty()) {
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if(map.containsKey("spinach") && !map.get("spinach").isEmpty()) {
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.*;
+
 public class Map2 {
+    public static Map<String, Integer> wordLen_kate(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String str: strings) {
+            if(!map.containsKey(str)) {
+                map.put(str, str.length());
+            }
+        }
+        return map;
+    }
 }
