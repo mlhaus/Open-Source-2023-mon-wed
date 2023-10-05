710ead7aefcf1386f5a32774368b973ee363692d
Chris Baenziger <chrisbaenziger@gmail.com>
2023-10-05 14:34:08 -0500

    Chris's Map1 and Map2

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,21 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+
+  public Map<String, String> mapAB4_chris(Map<String, String> map) {
+    if (map.containsKey("a") && map.containsKey("b")) {
+      String a = map.get("a");
+      String b = map.get("b");
+      if      (a.length() > b.length()) map.put("c", a);
+      else if (b.length() > a.length()) map.put("c", b);
+      else {
+        map.put("a", "");
+        map.put("b", "");
+      }
+    }
+    return map;
+  }
+
 }
