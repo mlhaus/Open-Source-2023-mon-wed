feffc3277a88b8ac8294c0f40a006f5313f84b03
James Williams <james-williams7@student.kirkwood.edu>
2023-10-05 19:14:36 -0500

    Added firstChar_james to Map2

--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
 public class Map2 {
+    public Map<String, String> firstChar_james(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for(String word : strings) {
+            String firstLetter = String.valueOf(word.charAt(0));
+            if(!map.containsKey(firstLetter)) {
+                map.put(firstLetter, word);
+            } else {
+                map.put(firstLetter, map.get(firstLetter) + word);
+            }
+        } return map;
+    }
 }
