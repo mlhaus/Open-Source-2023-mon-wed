39d73eff657071a4c0b520b5afab0b2044551e01
James Williams <james-williams7@student.kirkwood.edu>
2023-10-05 19:12:18 -0500

    Added topping1_james to Map1

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,13 @@
 package com.kirkwood.contribution2;
 
 public class Map1 {
+    public Map<String, String> topping1_james(Map<String, String> map) {
+        map.put("bread", "butter");
+        if(map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        return map;
+    }
+
 }
+
