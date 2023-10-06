4857bd628a57ed05cef72bb5e8e3330162c70e67
Mohmed Ahmed <mo7amedaziz1@gmail.com>
2023-10-06 13:55:04 -0500

    Complete Contribution 2 (CodingBat Map-1 & Map-2).

--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,14 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static Map<String, Integer> wordLen_mohmed(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : strings) {
+            map.put(str, str.length());
+        }
+        return map;
+    }
 }
