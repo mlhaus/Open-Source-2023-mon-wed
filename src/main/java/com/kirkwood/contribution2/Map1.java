979b724422b585719ce00b9c47a50f6825764446
Wyatt Asher <wspidermonkey@gmail.com>
2023-10-06 17:54:45 -0500

    Adding the codingbat classes map1 and map2

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,13 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapAB_Wyatt(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")){
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
+
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static Map<String, String> pairs_Wyatt(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for(String string : strings){
+            map.put(String.valueOf(string.charAt(0)), String.valueOf(string.charAt(string.length() - 1)));
+        }
+        return map;
+    }
+
 }
