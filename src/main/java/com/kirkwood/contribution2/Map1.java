77426615cf3bb17b3047e4761ce414cebd2f03d8
Julien Metrot <jul.metrot@gmail.com>
2023-10-09 12:46:17 -0500

    Julien's codingbat Map1 and Map2 challenges

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,13 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> mapShare(Map<String, String> map) {
+        map.remove("c");
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        return map;
+    }
 }
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,17 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
+    public static Map<String, Integer> wordLen(String[] strings) {
+        Map<String, Integer> counter = new HashMap<>();
+        for (String str: strings) {
+            if (!counter.containsKey(str)) {
+                counter.put(str, str.length());
+            }
+        }
+        return counter;
+    }
+
 }
