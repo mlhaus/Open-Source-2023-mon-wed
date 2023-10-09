536f834556d86e625e674ccfe6a98f61e39fe9af
Marc Hauschildt <marc.hauschildt@gmail.com>
2023-10-09 12:55:04 -0500

    Edit Map1

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,16 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public static Map<String, String> topping3_marc(Map<String, String> map) {
+        if(map.containsKey("potato") && !map.get("potato").equals("")) {
+            map.put("fries", map.get("potato"));
+        }
+        if(map.containsKey("salad") && !map.get("salad").equals("")) {
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
+
 }
