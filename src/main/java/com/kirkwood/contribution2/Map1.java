1808aec4101ff5ba3a9038a244b3c95173095ac3
Akoi Kollie <akollie2021@gmail.com>
2023-10-09 11:22:10 -0500

    A complete of codebat contribution exercise for map1 and map2

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,11 @@
 package com.kirkwood.contribution2;
 
-public class Map1 {
+public class Map<String String> mapBullyH_Akoi(Map<String, String> map){
+    String aStuff = map.get("a");
+    if(map.get("a") != null && !aStuff.equals("")) {
+        map.put("b", aStuff);
+        map.put("a", "");
+    }
+    return map;
 }
+
