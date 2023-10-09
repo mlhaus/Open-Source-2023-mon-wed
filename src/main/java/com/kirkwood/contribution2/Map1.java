a57bd91e5f26d6afa9d65217af8eca6a055af03b
RYALIE  CUARY <RCUARY7@GMAIL.COM>
2023-10-09 12:19:55 -0500

    Coding Bat contribution

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,4 +1,15 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
+    public Map<String, String> mapAB3_ryalie(Map<String, String> map) {
+        if(map.containsKey("a") && !map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        }
+        if(!map.containsKey("a") && map.containsKey("b")) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
 }
