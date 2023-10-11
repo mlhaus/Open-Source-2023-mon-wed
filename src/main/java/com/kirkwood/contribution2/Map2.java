11ad6ef998daa419f3ae8b42f2f956a618fb6665
miyada abas <miyadaabas@student.kirkwood.edu>
2023-10-11 12:37:26 -0500

    Map 1 $ Map 2

--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,4 +1,13 @@
 package com.kirkwood.contribution2;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Map2 {
+    public Map<String, String> pairs(String[] strings) {
+        Map<String, String> map = new HashMap();
+        for (String s:strings) {
+            map.put(s.charAt(0) + "", s.charAt(s.length() - 1) + "");
+        }
+        return map;
+    }
+
 }
