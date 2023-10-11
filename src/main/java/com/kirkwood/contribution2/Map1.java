7579a00ed33ba57b0baa9b2be7fe29eed1337832
Jared <jared.harvey10@gmail.com>
2023-10-11 12:26:45 -0500

    commit everything

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -2,7 +2,7 @@ package com.kirkwood.contribution2;
 
 import java.util.*;
 import java.util.Map;
-import java.util.Map;
+import java.util.HashMap;
 
 public class Map1 {
     public Map<String, String> mapAB3_ryalie(Map<String, String> map) {
@@ -120,10 +120,14 @@ public class Map1 {
         return map;
     }
 
-    public static Map<String, String> mapAB3_jared(Map<String, String> map) {
+    public Map<String, String> mapAB3_jared(Map<String, String> map) {
         if(map.containsKey("a") && !map.containsKey("b")) {
             map.put("b", map.get("a"));
         } else if(map.containsKey("b") && !map.containsKey("a")) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
           
           
     public Map<String, String> topping2_andrew(Map<String, String> map) {
