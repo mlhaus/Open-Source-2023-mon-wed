91417402eea3432ab47fdc23c51293a5a3956572
Julien Metrot <jul.metrot@gmail.com>
2023-10-16 13:59:43 -0500

    HacktoberFest Contribution 3 - Julien Metrot

---
+++ src/test/java/com/kirkwood/contribution2/Map2Test_Julien.java
@@ -0,0 +1,29 @@
+package com.kirkwood.contribution2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Map2Test_Julien {
+
+    @Test
+    void wordLen_julien() {
+        String[] arr1 = {"this", "and", "that", "and"};
+        Map<String, Integer> map1 = Map2.wordLen_julien(arr1);
+        assertEquals(4, map1.get("this"));
+        assertEquals(3, map1.get("and"));
+        assertEquals(4, map1.get("that"));
+
+        String[] arr2 = {"code", "code", "code", "bug"};
+        Map<String, Integer> map2 = Map2.wordLen_julien(arr2);
+        assertEquals(4, map2.get("code"));
+        assertEquals(3, map2.get("bug"));
+
+        String[] arr3 = {};
+        Map<String, Integer> map3 = Map2.wordLen_julien(arr3);
+        assertNull(map3.get(""));
+    }
+}
