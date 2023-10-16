ced5d68b02825ef6955ed7dc0995369876f7cd88
James Williams <james-williams7@student.kirkwood.edu>
2023-10-16 13:25:23 -0500

    Completed Map2 topping1_james unit tests AND made Map2 firstChar_james static

--- src/test/java/com/kirkwood/contribution2/Map2Test_James.java
+++ src/test/java/com/kirkwood/contribution2/Map2Test_James.java
@@ -2,11 +2,22 @@ package com.kirkwood.contribution2;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Map2Test_James {
 
     @Test
     void firstChar_james() {
+        //firstChar(["aa", "bb", "cc", "aAA", "cCC", "d"])
+        // → {"a": "aaaAA", "b": "bb", "c": "cccCC", "d": "d"}
+        String[] str = {"aa", "bb", "cc", "aAA", "cCC", "d"};
+        Map<String, String> map = Map2.firstChar_james(str);
+        assertEquals("aaaAA", map.get("a"));
+        assertEquals("bb", map.get("b"));
+        assertEquals("cccCC", map.get("c"));
+        assertEquals("d", map.get("d"));
     }
 }
