04597055678f870cf0b8860d8c55eef9dca4d073
Andrew Larson <andrew-larson3@student.kirkwood.edu>
2023-10-16 13:59:08 -0500

    Map1Test complete, still working on Map2

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -196,7 +196,7 @@ public class Map1 {
         return map;
     }
   
-    public Map<String, String> topping2_andrew(Map<String, String> map) {
+    public static Map<String, String> topping2_andrew(Map<String, String> map) {
         if (map.containsKey("ice cream")) {
             map.put("yogurt", map.get("ice cream")); // Set "yogurt" to the value of "ice cream"
         }
