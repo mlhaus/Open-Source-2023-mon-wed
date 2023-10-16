515f211a39d81f5f47d232adf0f3c8209b58b1ee
Tyler Barz <tyler-barz@student.kirkwood.edu>
2023-10-16 13:17:14 -0500

    map tests

--- src/main/java/com/kirkwood/contribution2/Map1.java
+++ src/main/java/com/kirkwood/contribution2/Map1.java
@@ -1,5 +1,7 @@
 package com.kirkwood.contribution2;
 
+import java.util.Map;
+
 public class Map1 {
     public static Map<String, String> mapAB4_tyler(Map<String, String> map) {
         if(map.containsKey("a") && map.containsKey("b")){
--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -1,5 +1,8 @@
 package com.kirkwood.contribution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
     public static Map<String, String> pairs_tyler(String[] strings) {
         Map<String, String> map = new HashMap<String, String>();
