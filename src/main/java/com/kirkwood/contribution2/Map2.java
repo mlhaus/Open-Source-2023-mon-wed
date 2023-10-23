b94d985a6ec4a65aac4850fe536f08be909ed5ae
Akoi Kollie <akollie2021@gmail.com>
2023-10-23 09:07:50 -0500

    A complete of codebat map2 and test2

--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -2,13 +2,14 @@ package com.kirkwood.contribution2;
 import java.util.*;
 public class Map2{
 
-    public static Map <String, Integer> wordLen_Akoi(String[] strings){
-        Map<String, Integer> map=new HashMap();
+    public static Map <String, Integer> wordLen_Akoi(String[]  strings){
+        Map<String, Integer> map = new HashMap();
         for(String s:strings){
             map.put(s,s.length());
         }
         return map;
     }
+
 
 }
 
