66f543164aac0d68e519f40e52b292f1215c463d
Donald Winchester <dwinchester0511@gmail.com>
2023-10-30 12:28:22 -0500

    Map1Test and Map2Test -Donald

---
+++ src/test/java/com/kirkwood/contribution2/Map2Test_donald.java
@@ -0,0 +1,15 @@
+package com.kirkwood.contribution2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Map2Test_donald {
+
+    @Test
+    void wordCount_donald() {
+        Map<String, Integer> counter = new HashMap<>();
+    }
+}
