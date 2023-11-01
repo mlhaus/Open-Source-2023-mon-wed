72eed672708cf993cfb9eee6ec40afdb1e8aeb65
Donald Winchester <dwinchester0511@gmail.com>
2023-11-01 12:03:12 -0500

    Revised Map2Test -Donald

--- src/test/java/com/kirkwood/contribution2/Map2Test_donald.java
+++ src/test/java/com/kirkwood/contribution2/Map2Test_donald.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-
 class Map2Test_donald {
 
     @Test
-    void wordCount_donald() {
-        Map<String, Integer> counter = new HashMap<>();
-    }
+    public static Map<String, Integer> wordCount_donald(String[] strings) {
+            Map<String, Integer> counter = new HashMap<>();
+            for(String str: strings) {
+                if(!counter.containsKey(str)) {
+                    counter.put(str, 1);
+                } else {
+                    counter.put(str, counter.get(str) + 1);
+                }
+            }
+            return counter;
+        }
 }
