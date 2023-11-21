1e4bab648d4d857b4e7b3c05327e8d73acce51e0
Wyatt Asher <wspidermonkey@gmail.com>
2023-11-21 12:33:12 -0600

    xml

--- src/main/java/com/kirkwood/contribution2/Map2.java
+++ src/main/java/com/kirkwood/contribution2/Map2.java
@@ -105,7 +105,11 @@ public class Map2 {
     public static Map<String, String> pairs_Wyatt(String[] strings) {
         Map<String, String> map = new HashMap<>();
         for (String string : strings) {
-            map.put(String.valueOf(string.charAt(0)), String.valueOf(string.charAt(string.length() - 1)));
+            if(string != null) {
+                map.put(String.valueOf(string.charAt(0)), String.valueOf(string.charAt(string.length() - 1)));
+            } else {
+                break;
+            }
         }
         return map;
     }
